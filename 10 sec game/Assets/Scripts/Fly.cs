018fb457fc6c6c481ea9f8dd5574665bbd91ade6
RobbyGman <rguzman97@knights.ucf.edu>
2023-01-22 15:34:43 -0500

    commit

--- 10 sec game/Assets/Scripts/Fly.cs
+++ 10 sec game/Assets/Scripts/Fly.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Manage : MonoBehaviour
+public class Fly : MonoBehaviour
 {
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            rb.velocity= Vector2.up * 10;
+        }
         
     }
 }
